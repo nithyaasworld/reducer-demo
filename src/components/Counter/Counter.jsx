fe2241da36a4a139c71acbba35aff51f4b042e59
Nithya <nithyaasworld@gmail.com>
2021-05-19 01:42:45 +0530

    formatted the docs

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,16 +1,25 @@
-import counterStyles from './Counter.module.css';
-import CounterContext from '../CounterContext/CounterStore';
-import { useContext } from 'react';
-
+import counterStyles from "./Counter.module.css";
+import CounterContext from "../CounterContext/CounterStore";
+import { useContext } from "react";
 
 export default function Counter() {
-    let { counterState, dispatch } = useContext(CounterContext);
-    return (
-        <div className={counterStyles['counter-container']}>
-            <h2>Counter Buttons</h2>
-            <button className={counterStyles.btn}  onClick={()=> dispatch('decrement')}>-</button>
-            {counterState.counterValue}
-            <button className={counterStyles.btn}  onClick={()=> dispatch('increment')}>+</button>
-        </div>
-    )
+  let { counterState, dispatch } = useContext(CounterContext);
+  return (
+    <div className={counterStyles["counter-container"]}>
+      <h2>Counter Buttons</h2>
+      <button
+        className={counterStyles.btn}
+        onClick={() => dispatch("decrement")}
+      >
+        -
+      </button>
+      {counterState.counterValue}
+      <button
+        className={counterStyles.btn}
+        onClick={() => dispatch("increment")}
+      >
+        +
+      </button>
+    </div>
+  );
 }
--- src/components/CounterContext/CounterStore.jsx
+++ src/components/CounterContext/CounterStore.jsx
@@ -1,32 +1,34 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer } from "react";
 
 let CounterContext = createContext({
-    counterValue: 0
+  counterValue: 0,
 });
 
 export default CounterContext;
 
 export function CounterContextProvider(props) {
-    let reducer = (state, action) => {
-        switch (action) {
-            case 'increment':
-                return { counterValue: state.counterValue + 1 };
-            case 'decrement':
-                return { counterValue: state.counterValue - 1 };
-            case 'reset':
-                return { counterValue: 0 };
-            default:
-                console.log(`Invalid ${action}`);
-        }
+  let reducer = (state, action) => {
+    switch (action) {
+      case "increment":
+        return { counterValue: state.counterValue + 1 };
+      case "decrement":
+        return { counterValue: state.counterValue - 1 };
+      case "reset":
+        return { counterValue: 0 };
+      default:
+        console.log(`Invalid ${action}`);
     }
-    let initialState = { counterValue: 0 };
-    let [counterState, dispatch] = useReducer(reducer, initialState);
-    return (
-        <CounterContext.Provider value={{
-            counterState,
-            dispatch
-        }} >
-            {props.children}
-        </CounterContext.Provider>
-    )
+  };
+  let initialState = { counterValue: 0 };
+  let [counterState, dispatch] = useReducer(reducer, initialState);
+  return (
+    <CounterContext.Provider
+      value={{
+        counterState,
+        dispatch,
+      }}
+    >
+      {props.children}
+    </CounterContext.Provider>
+  );
 }
